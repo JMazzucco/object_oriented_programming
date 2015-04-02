94eb11883a83b2684f89d7ff0261d1c9968f9772
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-02 17:03:20 -0400

    people.rb: added initialize methods to th Student and Instructor classes

--- people.rb
+++ people.rb
@@ -1,9 +1,26 @@
-class Student
+
+class Student < Parent
+	def initialize(name)
+		@name = name
+	end
+
 	def learn
 		"I get it"
 	end
 end
 
 class Instructor
+	def initialize(name)
+		@name = name
+	end
+	def teach
+		"everything in Ruby is an object"
+	end
 end
 
+class Parent
+end
+
+
+test = Student.new
+puts test.learn
