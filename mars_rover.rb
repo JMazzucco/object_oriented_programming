bba6a43672d7686cbc851859e4e396310c2359fc
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-03 12:06:18 -0400

    mars_rover: previously the x and y variables were assigned the first and last digits of the coordinate string entered by the user. This is an issue as a coorinate can have more than one digit. This has been fixed.

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,12 +1,12 @@
-class Rover
-	def initialize(x, y, direction)
-		@x = x
-		@y = y
-		puts x
-		puts y
-		@direction = direction
-		puts direction
-	end
+# class Rover
+# 	def initialize(x, y, direction)
+# 		@x = x
+# 		@y = y
+# 		puts x
+# 		puts y
+# 		@direction = direction
+# 		p direction
+# 	end
 
 	# def read_instructions
 	# 	#tell rover to move or turn - delegates to move and turn methods
@@ -27,25 +27,25 @@ class Rover
 	# 	puts "turn method called"
 	# end
 
-end
+#end
 
 
-#puts "type upper-right coordinates of plateau"
-#plat_limit = gets.chomp.split('')
-#x_limit = plat_limit.first
-#y_limit = plat_limit.last
+# puts "type upper-right coordinates of plateau"
+# plat_limit = gets.chomp.split('')
+# x_limit = plat_limit.first
+# y_limit = plat_limit.last
 
-# puts "type current coordinates for Rover"
-# plat_input = gets.chomp.split('')
-#x = plat_input.first
-#y = plat_input.last
+puts "type current coordinates for Rover"
+plat_input = gets.chomp.split
+
+x = plat_input[0].to_i
+y = plat_input[1].to_i
+p plat_input
+puts x
+puts y
 
 # puts "type instructions for Rover"
 # direction = gets.chomp.downcase.split('')
 
-ex = 3
-why = 6
-direction = ["m", "l", "r", "m"]
 
-new_rover = Rover.new(ex, why, direction)
-new_rover
+#Rover.new(x, y, direction)
