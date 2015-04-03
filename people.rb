d152c84b2133d9ea7ab174cafb7de32548bd4343
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-02 21:52:58 -0400

    people.rb: called remaining methods in the Student and Instructor classe

--- people.rb
+++ people.rb
@@ -1,42 +1,38 @@
-
+#assign a name that is passed as an instance variable
 class Person
 	def initialize(name)
 		@name = name
 	end
+	def greeting
+		"Hi, my name is #{@name}"
+	end
 end
 
-
+#inherit methods from Person class
 class Student < Person
-
 	def learn
 		"i get it!"
 	end
-
-	def greeting
-		"Hi, my name is #{@name}"
-	end
-
 end
 
+#inherit methods from Person class
 class Instructor < Person
-
 	def teach
 		"Everything in Ruby is an Object"
 	end
-
-	def greeting
-		"Hi, my name is #{@name}"
-	end
-
 end
 
-
-
+#create new instances of the Student and Instructor classes
 new_inst = Instructor.new("Chris")
-puts new_inst.greeting
-
 new_student = Student.new("Cristina")
+
+#call the greeting method in both instances and display the returned values
+puts new_inst.greeting
 puts new_student.greeting
 
+#call the remaining method in each instance
 puts new_inst.teach
 puts new_student.learn
+
+#calling the teach method on an instance of the Student class does not work as the method is encapsulated in the Instructor class and is not accessible from other classes - it would work if Instructor was a parent class of Student.
+#puts new_student.teach
