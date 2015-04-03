8cab445f1768bc2c5c326a42630fab73dc35bbdd
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-03 14:11:29 -0400

    coordinate input must contain two integers before it is passed to the Rover class, and all characters and extra spaces are removed

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,33 +1,31 @@
-# class Rover
-# 	def initialize(x, y, direction)
-# 		@x = x
-# 		@y = y
-# 		puts x
-# 		puts y
-# 		@direction = direction
-# 		p direction
-# 	end
+class Rover
+	def initialize(x, y)
+		@x = x
+		@y = y
+	#@direction = direction
+	puts @x
+	puts @y
+	end
 
-	# def read_instructions
-	# 	#tell rover to move or turn - delegates to move and turn methods
-	# 	@direction.each do |x|
-	# 		move if x == "m"
-	# 		turn if x == "l" || x =="r"
-	# 	end
-	# end
+# 	# def read_instructions
+# 	# 	#tell rover to move or turn - delegates to move and turn methods
+# 	# 	@direction.each do |x|
+# 	# 		move if x == "m"
+# 	# 		turn if x == "l" || x =="r"
+# 	# 	end
+# 	# end
 
-	# def move
-	# 	#when an "m" is passed, this method affects the position of the rover, then updates co-orinates.
-	# 	puts "move method called"
+# 	# def move
+# 	# 	#when an "m" is passed, this method affects the position of the rover, then updates co-orinates.
+# 	# 	puts "move method called"
 
-	# end
+# 	# end
 
-	# def turn
-	# 	#turns rover when a L or R is passed. Checks current position (direction), turns rover once, then updates the direction parameter.
-	# 	puts "turn method called"
-	# end
-
-#end
+# 	# def turn
+# 	# 	#turns rover when a L or R is passed. Checks current position (direction), turns rover once, then updates the direction parameter.
+# 	# 	puts "turn method called"
+# 	# end
+end
 
 
 # puts "type upper-right coordinates of plateau"
@@ -35,17 +33,32 @@
 # x_limit = plat_limit.first
 # y_limit = plat_limit.last
 
+#remove characters from passed string, then split numbers into an array and convert them to integers
+def coord_crop(coord_str)
+	crop_char = coord_str.gsub(/[A-Za-z]/, " ")
+	crop_sym = coord_str.gsub(/[^0-9]/, " ")
+	crop_space = crop_sym.gsub(/\s+/, " ")
+	coord_array = crop_space.split
+	coord_array.collect! {|x| x.to_i }
+end
+
+#prompt user to enter coordinates, then pass string to the coord_crop method
 puts "type current coordinates for Rover"
-plat_input = gets.chomp.split
+coord_edited = coord_crop(gets.chomp)
 
-x = plat_input[0].to_i
-y = plat_input[1].to_i
-p plat_input
-puts x
-puts y
+#prompt user for coordinates until an array of 2 integers is returned
+until coord_edited.count == 2
+	puts "Invalid coordinates. Please try again."
+	coord_edited = coord_crop(gets.chomp)
+end
 
-# puts "type instructions for Rover"
+#assign the first number in the array as x, and the second as y
+x = coord_edited[0]
+y = coord_edited[1]
+
+#puts "type instructions for Rover"
 # direction = gets.chomp.downcase.split('')
+# #p direction
 
-
-#Rover.new(x, y, direction)
+#create a new instance of the Rover class and pass it the x and y coordinates
+Rover.new(x, y)
