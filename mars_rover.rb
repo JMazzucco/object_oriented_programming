9d8217be17e86a4d4bf5d2750c9ca9d6869a8633
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-03 17:23:03 -0400

    mars_rover: accepts exactly 2 upper-limit coordinates and removes characters

--- mars_rover.rb
+++ mars_rover.rb
@@ -21,27 +21,21 @@ class Rover
 
 	def move
 		#when an "m" is passed, this method affects the position of the rover, then updates co-orinates.
-
-
 		if @y > 0 && (@current_dir == "N" || @current_dir == "S")
 				if @current_dir == "N"
 					@y += 1
 				elsif @current_dir == "S"
 					@y -= 1
 				end
-
 		elsif @x > 0 && (@current_dir == "W" || @current_dir == "E")
 				if @current_dir == "W"
 					@x -= 1
 				elsif @current_dir == "E"
 					@x += 1
 				end
-
 		else
 				puts "The rover is at the edge of the plateau, and it cannot move futher #{@current_dir}"
 		end
-
-		puts "x:#{@x}, y:#{@y}"
 	end
 
 	def turn
@@ -72,16 +66,9 @@ class Rover
 		end
 	end
 
-	def check_position
-	end
-
 end
 
 
-# puts "type upper-right coordinates of plateau"
-# plat_limit = gets.chomp.split('')
-# x_limit = plat_limit.first
-# y_limit = plat_limit.last
 
 #remove characters from passed string, then split numbers into an array and convert them to integers
 def coord_crop(coord_str)
@@ -89,7 +76,15 @@ def coord_crop(coord_str)
 	crop_sym = coord_str.gsub(/[^0-9]/, " ")
 	crop_space = crop_sym.gsub(/\s+/, " ")
 	coord_array = crop_space.split
-	coord_array.collect! {|x| x.to_i }
+	coord_edited = coord_array.collect! {|x| x.to_i }
+
+	#prompt user for coordinates until an array of 2 integers is returned
+ 	if coord_edited.count != 2
+		puts "Invalid coordinates. Please try again."
+		coord_crop(gets.chomp)
+	else
+		return coord_edited
+	end
 end
 
 #only keep characters in passed string if they are "r", "l" or "m"
@@ -98,23 +93,21 @@ def direction_crop(direction_str)
 	direction_array.keep_if {|d| d == "r" || d == "l" || d == "m"}
 end
 
+puts "type upper-right coordinates of plateau"
+plat_limit = coord_crop(gets.chomp)
+x_limit = plat_limit[0]
+y_limit = plat_limit[1]
+
 #prompt user to enter coordinates, then pass string to the coord_crop method
 puts "type current coordinates for Rover"
-coord_edited = coord_crop(gets.chomp)
-
-#prompt user for coordinates until an array of 2 integers is returned
-until coord_edited.count == 2
-	puts "Invalid coordinates. Please try again."
-	coord_edited = coord_crop(gets.chomp)
-end
-
-#assign the first number in the array as x, and the second as y
-x = coord_edited[0]
-y = coord_edited[1]
+coord_final = coord_crop(gets.chomp)
+x = coord_final[0]
+y = coord_final[1]
 
 #prompt user for rover
 puts "type instructions for Rover"
 direction = direction_crop(gets.chomp)
+
 
 #create a new instance of the Rover class and pass it the x and y coordinates
 rover_new = Rover.new(x, y, direction)
