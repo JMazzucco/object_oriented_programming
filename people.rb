1059bc5184f3ec32dac14ae054bd02ab6cf0bbd8
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-02 21:13:27 -0400

    people.rb: called the teach method in the Instructor class and the learn method in the Student class

--- people.rb
+++ people.rb
@@ -1,12 +1,12 @@
 
-class Parent
+class Person
 	def initialize(name)
 		@name = name
 	end
 end
 
 
-class Student < Parent
+class Student < Person
 
 	def learn
 		"i get it!"
@@ -18,7 +18,7 @@ class Student < Parent
 
 end
 
-class Instructor < Parent
+class Instructor < Person
 
 	def teach
 		"Everything in Ruby is an Object"
@@ -32,8 +32,11 @@ end
 
 
 
-new_inst = Instructor.new("test")
+new_inst = Instructor.new("Chris")
 puts new_inst.greeting
 
-new_student = Student.new("test2")
+new_student = Student.new("Cristina")
 puts new_student.greeting
+
+puts new_inst.teach
+puts new_student.learn
