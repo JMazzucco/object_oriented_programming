15d77e42a7373bd52f409d39a68a605ce1733072
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-04 11:23:42 -0400

    sales_taxes: calculates total amount, excluding taxes

--- sales_taxes.rb
+++ sales_taxes.rb
@@ -1,27 +1,53 @@
-# sales_tax = 0.10
-# duty_tax = 0.05
 
-# basket_one = {
-# "book" => 12.49,
-# "music CD" => 14.99,
-# "chocolate bar" => 0.85,
-# }
+
 
 shopping_basket = Hash.new
 new_item = gets.chomp
 
 until new_item == ""
 item_name = new_item.gsub(/[0-9]/, '').gsub('at', '').gsub(/[^a-z]/, '').strip
-item_price = new_item[-5..-1].strip
-item_quant = new_item[0]
+item_price = new_item[-5..-1].strip.to_f
+#first number (i.e. item quantity) in new_item string cannot be over 99
+item_quant = new_item[0..1].strip.to_i
 
-puts "#{item_quant} #{item_name} : #{item_price}"
+basket_key = "#{item_quant} #{item_name}".to_s
 
+item_subtotal = (item_price * item_quant)
+# puts item_subtotal
+
+shopping_basket[basket_key] = item_subtotal
+
+
+# puts "#{item_quant} #{item_name} : #{item_price}"
 
 new_item = gets.chomp
-
 end
 
+# sales_tax = 0.10
+# duty_tax = 0.05
+no_tax_total = 0
+shopping_basket.each {|k, v| no_tax_total += v}
+puts no_tax_total
+
+
+
+# total_tax = 0
+# shopping_basket.each do |k, v|
+# 		unless medical.has_key(k) || books.has_key(k) || food.has_key(k)
+# 			total_tax = v * 0.10
+# 		end
+
+# 		if imports.has_key(k)
+# 			total_tax = v * 0.05
+# 		end
+# end
+
+
+# basket_one = {
+# "book" => 12.49,
+# "music CD" => 14.99,
+# "chocolate bar" => 0.85,
+# }
 
 # basket_two = {
 # "imported box of chocolates" => 10.00,
