406e9bfd08805342c0e8a4ae306c63f19d8affdc
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-03 21:07:07 -0400

    sales_taxes: created multiple hashes to categorize the products.

--- sales_taxes.rb
+++ sales_taxes.rb
@@ -1,15 +1,33 @@
-
-
 sales_tax = 0.10
 duty_tax = 0.05
 
-products = {
-	"book" => 12.49
+misc = {
 	"music CD" => 14.99
+	"hat" => 10.15
+	"sweater" => 30.99
+}
+
+food = {
 	"chocolate bar" => 0.85
+	"bananas" => 0.35
+	"papaya" => 4.50
+}
+
+medical {
+	"packet of headache pills" => 9.75
+	"first-aid kit" => 20.50
+	"ketosticks" => 10.50
+}
+
+books = {
+	"book" => 12.49
+	"man and his symbols" => 10.50
+	"bulletproof diet" => 15.50
+}
+
+imports = {
 	"imported box of chocolates" => 10.00
 	"imported bottle of perfume" => 47.50
-	"packet of headache pills" => 9.75
 	"box of imported chocolates" => 11.25
 }
 
