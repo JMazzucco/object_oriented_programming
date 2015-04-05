5002a924fc7dd0ad0f36b6c85c7408d9170bcfbc
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-05 09:32:37 -0400

    sales_taxes: tax amounts round up to the nearest 5 cents.

--- sales_taxes.rb
+++ sales_taxes.rb
@@ -1,53 +1,63 @@
 
+#sales taxes are excluded if one or more of the following keywords are in item_name
 medical = ["eyedrops", "lozenges", "pills"]
 food = ["banana", "apples", "chocolate"]
+
+def round_5(amount)
+	amount = ((amount * 100).to_i).to_f/100
+	unless (amount % 0.05 == 0)
+		if amount.to_s[-1].to_i > 5
+			return amount.round(1)
+		else
+			return ((amount * 10).to_i.to_f/10) + 0.05
+		end
+	end
+	return amount
+end
 
 shopping_basket = Hash.new
 new_item = gets.chomp
 
 until new_item == ""
-item_name = new_item.gsub(/[0-9]/, '').gsub('at', '').gsub(/[^a-z]/, ' ').strip
-item_price = new_item[-5..-1].strip.to_f
-
-#first number (i.e. item quantity) in new_item string cannot be over 99
-item_quant = new_item[0..1].strip.to_i
-
-basket_key = "#{item_quant} #{item_name}"
-
-item_subtotal = (item_price * item_quant)
-# puts item_subtotal
-
-shopping_basket[basket_key] = item_subtotal
-
-
-# puts "#{item_quant} #{item_name} : #{item_price}"
-
-new_item = gets.chomp
+	item_name = new_item.gsub(/[0-9]/, '').gsub('at', '').gsub(/[^a-z]/, ' ').strip
+	item_price = new_item[-5..-1].strip.to_f
+	#first number (i.e. item quantity) in new_item string cannot be over 99
+	item_quant = new_item[0..1].strip.to_i
+	basket_key = "#{item_quant} #{item_name}"
+	item_subtotal = (item_price * item_quant)
+	# puts item_subtotal
+	shopping_basket[basket_key] = item_subtotal
+	#puts "#{item_quant} #{item_name} : #{item_price}"
+	new_item = gets.chomp
 end
 
-no_tax_total = 0
-shopping_basket.each {|k, v| no_tax_total += v}
 
-total_tax = 0
+# shopping_basket.each {|k, v| no_tax_total += v}
+item_tax, item_sales_tax, item_duty_tax, total_tax, item_total, grand_total, tax_rounded = 0, 0, 0, 0, 0, 0, 0
+
 exclude_tax = false
-duty_flag = false
+duty_tax = false
 
 shopping_basket.each do |k, v|
 	k.split(' ').each do |x|
-		exclude_tax = true if medical.include?(x) || food.include?(x) || k.include?("book")
-		duty_flag = true if k.include?("imported")
-
+		exclude_tax = true if medical.include?(x) || food.include?(x) || x.include?("book")
+		duty_tax = true if x.include?("imported")
 	end
-	total_tax += (v * 0.10) unless exclude_tax
-	total_tax += (v * 0.05) if duty_flag
 
+	item_sales_tax = (v * 0.10) unless exclude_tax
+	item_duty_tax = (v * 0.05) if duty_tax
+
+	item_tax = (item_sales_tax + item_duty_tax).to_f
+ 	puts item_tax
+  puts round_5(item_tax)
+
+
+
+	# total_tax += tax_rounded
+	# item_total += (tax_rounded + v)
+	# grand_total += item_total
+	# puts "#{k} : #{item_total}"
 end
-puts total_tax
 
-
-
-
-
-
-
-
+puts "Sales Tax: #{total_tax}"
+puts "Total: #{grand_total}"
